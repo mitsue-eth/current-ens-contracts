9842df97bce6c9035f015a61bd9ea66d53104704
Mitsue <mitsue.eth@gmail.com>
2022-09-26 17:36:18 +0200

    added ens hashing utilities

---
+++ utils/hashing-normalization.js
@@ -0,0 +1,22 @@
+const ethers = require("ethers");
+const BigNumber = ethers.BigNumber;
+const utils = ethers.utils;
+const namehash = require("@ensdomains/eth-ens-namehash");
+
+const stringToNormalize = "MITsue.eth";
+const stringToNamehash = "mitsue.eth";
+const labelForTokenId = "vitalik";
+
+//normalize NameHash
+const normalized = namehash.normalize(stringToNormalize);
+const hash1 = namehash.hash(normalized);
+console.log(`Normalized namehash of ${stringToNormalize}: `, hash1);
+
+//calculate Namehash
+const hash = namehash.hash(stringToNamehash);
+console.log(`Namehash of ${stringToNamehash}:`, hash);
+
+//calculate TokenId from its label
+const labelHash = utils.keccak256(utils.toUtf8Bytes(labelForTokenId));
+const tokenId = BigNumber.from(labelHash).toString();
+console.log(`TokenID for label ${labelForTokenId}:`, tokenId);
